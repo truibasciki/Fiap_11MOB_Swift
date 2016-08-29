65fe89351f2377021fd7df860b0e661eff4b9fec
Usuário Convidado <Guest@pa06micro32.local>
2016-08-29 20:25:20 -0300

    hidden jogador e quadra

--- thomasfiap/CadastroViewController.swift
+++ thomasfiap/CadastroViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class CadastroViewController: UIViewController {
 
+    @IBOutlet weak var segmentedJogQua: UISegmentedControl!
+    
+    @IBOutlet weak var NomePlayer: UILabel!
+    @IBOutlet weak var labelTelefone: UILabel!
+    
+    @IBOutlet weak var labelLatitude: UILabel!
+    @IBOutlet weak var labelLongitude: UILabel!
+    
+    
+    @IBOutlet weak var txtTelefone: UITextField!
+    @IBOutlet weak var txtLatitude: UITextField!
+    @IBOutlet weak var txtLongitude: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.addBackground();
+        
+        labelLatitude.hidden = true
+        labelLongitude.hidden = true
+        txtLatitude.hidden = true
+        txtLongitude.hidden = true
+        
 navigationItem.imageNavigation();
         // Do any additional setup after loading the view.
     }
@@ -26,11 +46,39 @@ navigationItem.imageNavigation();
     @IBOutlet weak var txtSenha: UITextField!
     
     @IBAction func trocaPerfil(sender: AnyObject) {
+        
+        if(segmentedJogQua.selectedSegmentIndex == 0) {
+            
+            NomePlayer.text = "Nome de Boleiro"
+            labelLatitude.hidden = true
+            labelLongitude.hidden = true
+            txtLatitude.hidden = true
+            txtLongitude.hidden = true
+            
+        } else {
+            
+            NomePlayer.text = "Nome da Quadra"
+            labelLatitude.hidden = false
+            labelLongitude.hidden = false
+            txtLatitude.hidden = false
+            txtLongitude.hidden = false
+        }
+        
     }
 
     @IBAction func logarFb(sender: AnyObject) {
     }
     @IBAction func cadastrar(sender: AnyObject) {
+        
+        if(segmentedJogQua.selectedSegmentIndex == 0) {
+            
+            
+        } else {
+            
+            
+            
+        }
+        
     }
     /*
     // MARK: - Navigation
